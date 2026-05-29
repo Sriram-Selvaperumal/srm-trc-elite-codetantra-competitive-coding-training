b637a69f9445cbc303e205211d747bb3f3549c7f
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2026-05-29 21:14:15 +0530

    Add upper bound search in sorted array
    Implement a program to find the upper bound of a target in a sorted array.

---
+++ Day 4/Medium/4.2.2. Upper Bound in a Sorted Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;cin >> n;
+	vector<int> arr(n);
+	bool found = false;
+	for(int i = 0 ; i < n ; i++)cin >> arr[i];
+	int target ; cin >> target;
+
+	for(int i = 0 ; i < n ; i++){
+		if(arr[i]>target){
+			cout << i;
+			found = true;
+			break;
+		}
+	}
+
+	if(!found)cout <<-1;
+	return 0;
+}
